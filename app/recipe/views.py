068f4ea5833e2b0d7be538fb05156fe608b6ec92
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-10-25 15:52:48 +0900

    finished recipe API tests and views

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,6 @@
 Views for the recipe APIs
 """
 
-from tokenize import Token
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
